0735b8a49143fac35905920cbcd8495fef34a8e9
sujon-ahmed-WD <sujon.mrist@gmail.com>
2024-12-25 09:28:43 +0600

    this is a linked list add

---
+++ md-5/Praties.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{ // class and object daoer uddoso holo ja node modda val+nextp aksatha asa ti ...
+public:
+    int val;
+    Node *next; // akna amier next pointer dara try kora hoycaa
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+int main()
+{
+    Node a(10), b(20), c(30);
+
+    a.next = &b;
+    b.next = &c;
+
+    cout << a.val << " " << b.val << " " << c.val << endl;
+    cout << (*a.next).val << endl;
+    return 0;
+}
---
+++ md-5/linked_list.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     vector<int>v={1,2,3,4,5};
+    cout<<(int)&v[2]<<" "<<(int)&v[3];
+    return 0;
+}
