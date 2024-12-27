8843579de64743d74d3a23c6f067d759c6c2c55c
sujon-ahmed-WD <sujon.mrist@gmail.com>
2024-12-27 12:06:32 +0600

    Printnge_linkedlis

--- md-5/Praties.cpp
+++ md-5/Praties.cpp
@@ -1,6 +1,28 @@
- #include<bits/stdc++.h>
- using namespace std;
- int main()
- {
-    return 0;
- }
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+   int val;
+   Node *next;
+   Node(int val)
+   {
+      this->val=val;
+      this->next=NULL;
+   }
+};
+int main()
+{
+   Node a(10), b(20), c(30);
+ 
+
+   a.next = &b;
+   b.next = &c;
+   // c.next = NULL;
+
+   cout << a.val << endl;
+   cout << (*a.next).val << endl;            
+   cout<<a.next->next->val<<endl;             
+
+   return 0;
+}
---
+++ md-5/printing_linked_list.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    // Node* d=new Node(40);
+
+    (*head).next=a;  
+    head->next = a;
+    a->next = b;
+
+    // cout << head->val << endl;
+    // cout << head->next->val << endl;
+    // cout << head->next->next->val << endl;
+    Node *temp = head;
+    while (temp!= NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+
+    return 0;
+}
